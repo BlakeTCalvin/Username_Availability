66139edc5b328590c4f43f76e26d944d5ea5ce57
Blake Calvin <btcalvin4@gmail.com>
2019-07-20 20:55:19 -0700

    installed marshmallow

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 import re
 
 app = Flask(__name__)
@@ -13,5 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+ma = Marshmallow(app)
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
